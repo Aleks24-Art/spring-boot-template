5ae7e29b732344f8a59a440d1c357a3f8263b420
aleksenko.as <aleksenko.as@esphere.ru>
2022-04-03 18:27:02 +0300

    Add example of spring aop

---
+++ src/main/java/com/example/template/aspect/LoggableAspect.java
@@ -0,0 +1,60 @@
+package com.example.template.aspect;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggableAspect {
+
+    @Pointcut("@annotation(Loggable)")
+    public void executeLogging() {
+    }
+
+    @Before("executeLogging()")
+    public void logMethodCall(JoinPoint joinPoint) {
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName()).append("!");
+        Arrays.stream(joinPoint.getArgs()).forEach(arg ->
+                message.append("args: ").append(arg).append("!")
+        );
+
+        log.info(message.toString());
+    }
+
+    @AfterReturning(pointcut = "executeLogging()", returning = "returnValue")
+    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName()).append("!");
+        message.append("return value: ").append(returnValue);
+
+        log.info(message.toString());
+    }
+
+    @SneakyThrows
+    @Around("executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) {
+        long start = System.currentTimeMillis();
+
+        Object returnValue = joinPoint.proceed();
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName()).append("!");
+
+        long finish = System.currentTimeMillis();
+
+        message.append("time: ").append(finish - start).append(" milliseconds");
+        log.info(message.toString());
+        return returnValue;
+    }
+}
